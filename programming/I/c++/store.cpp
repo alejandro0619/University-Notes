107c7759b66c8b374b07db2bb7febf884e26f579
Spaghetti <alejandrolpz0619@gmail.com>
2022-10-11 23:08:46 -0400

    11/10/22 Started re-writing the program, making it clearing. Still a lot of tod-do I'll be solving in the go.

--- programming/I/c++/store.cpp
+++ programming/I/c++/store.cpp
@@ -20,31 +20,46 @@
 */
 
 #include <iostream>
+#include <vector>
+
+
+
+struct Article {
+public:
+  std::string name;
+  float price;
+  int quantity;
+  std::string code;
+  int discount;
+
+};
+
+struct ListOfArticles {
+  std::vector<Article> article;
+};
 struct Menu {
 public:
   int op; // menu option
   bool isValid = true;
   // displays a menu with 6 options
   void display();
-  void executeOpt();
+  void executeOpt(Article article);
   void validInput();
 
-private:
-  void addArticles();
 };
 
 void Menu::display() {
-    int ans;
-    std::cout
-      << "1. Agregar articulo." << std::endl
-      << "2. Estadisticas (En BS)." << std::endl
-      << "3. Estadisticas (EN USD)." << std::endl
-      << "4. Configuracion (IVA y USD)." << std::endl
-      << "5. Limpiar registro." << std::endl
-      << "6. Salir." << std::endl;
-    std::cin >> ans;
-    op = ans;
- }
+  int ans;
+  std::cout
+    << "1. Agregar articulo." << std::endl
+    << "2. Estadisticas (En BS)." << std::endl
+    << "3. Estadisticas (EN USD)." << std::endl
+    << "4. Configuracion (IVA y USD)." << std::endl
+    << "5. Limpiar registro." << std::endl
+    << "6. Salir." << std::endl;
+  std::cin >> ans;
+  op = ans;
+}
 
 void Menu::validInput() {
   if (op >= 1 && op <= 6) {
@@ -55,47 +70,43 @@ void Menu::validInput() {
   }
 }
 
-void Menu::addArticles() {
-  std::cout << "Numero de articulos que deseas agregar." << std::endl;
-  
-}
-void Menu::executeOpt() {
+void Menu::executeOpt(Article article) {
   // If it's invalid re-render the menu
   while (isValid != 1) {
     std::cout << "Opcion incorrecta" << std::endl;
     Menu::display();
+    Menu::validInput();
   }
-   switch (op)
-   {
-   case 1:
-     Menu::addArticles();
-     break;
-   case 2:
-     
-     break;
-   case 3:
-     
-     break;
-   case 4:
-     
-     break;
-   case 5:
-     
-     break;
-   case 6:
-     
-     break;
-     
-   default:
+  switch (op)
+  {
+  case 1:
     break;
-   }
+  case 2:
+
+    break;
+  case 3:
+
+    break;
+  case 4:
+
+    break;
+  case 5:
+
+    break;
+  case 6:
+
+    break;
+
+  default:
+    break;
+  }
 }
 
 int main()
 {
-   Menu menu;
-   menu.display();
-   menu.validInput();
-   menu.executeOpt();
-   return 0;
+  Menu menu;
+  menu.display();
+  menu.validInput();
+  menu.executeOpt(Article());
+  return 0;
 }
